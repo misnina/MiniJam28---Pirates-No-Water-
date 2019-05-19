3dff1bac183064d6d888433d3ef85dfbb9238c85
Nina <ninarachaebuie@gmail.com>
2019-05-19 06:45:42 -0500

    addition of projectiles

---
+++ Assets/Scripts/Cannonball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannonball : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Vector3 direction = PlayerController.instance.transform.position - transform.position;
+        rb.AddForce(direction * speed);
+        StartCoroutine("Die");
+    }
+
+    IEnumerator Die()
+    {
+        yield return new WaitForSeconds(10);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/ShootCannon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootCannon : MonoBehaviour
+{
+    public GameObject ball;
+    public float inital;
+    public float rate;
+    public float speed;
+
+    void Start()
+    {
+        InvokeRepeating("Fire", inital, rate);
+    }
+
+    void Update()
+    {
+         Quaternion rotation = Quaternion.LookRotation(PlayerController.instance.transform.position - transform.position, transform.TransformDirection(Vector3.up));
+         transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+    }
+
+    private void Fire()
+    {
+        Cannonball newBall = Instantiate(ball, transform.position, transform.rotation).GetComponent<Cannonball>();
+        newBall.speed = speed;
+
+    }
+}
